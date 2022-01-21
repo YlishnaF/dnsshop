cfd76fbfac2728033a97221aa46831327468e719
YlishnaF <ylishnaf@yandex.ru>
2022-01-21 12:21:07 +0500

    first commit

---
+++ src/test/java/DnsTest.java
@@ -0,0 +1,27 @@
+import base.BaseClass;
+import com.google.errorprone.annotations.Var;
+import framework.data.VariablesForSearch;
+import org.junit.jupiter.api.Test;
+
+public class DnsTest extends BaseClass {
+
+    @Test
+    public void test() throws InterruptedException {
+        pageManager.getStartPage()
+                .startSearchProduct(VariablesForSearch.COMMON_PHONE_NAME)
+                .clickOnProduct(VariablesForSearch.SPECIFIC_PHONE_NAME)
+                .checkThePriceChangedWithGuarantee()
+                .buyProduct()
+                .newSearch(VariablesForSearch.COMMON_GAME_NAME)
+                .buyProduct(VariablesForSearch.SPECIFIC_GAME_NAME)
+                .goIntoCart()
+                .checkGuarantee(VariablesForSearch.GUARANTEE_DURATION)
+                .checkTotalCartPrice()
+                .deleteItemFromCart(VariablesForSearch.SPECIFIC_GAME_NAME)
+                .checkTotalCartPrice()
+                .addAdditionalItemToCart(VariablesForSearch.SPECIFIC_PHONE_NAME)
+                .addAdditionalItemToCart(VariablesForSearch.SPECIFIC_PHONE_NAME)
+                .checkTotalCartPrice();
+
+    }
+}
---
+++ src/test/java/base/BaseClass.java
@@ -0,0 +1,32 @@
+package base;
+
+import framework.managers.DriverManager;
+import framework.managers.InitManager;
+import framework.managers.PageManager;
+import framework.managers.TestPropManager;
+import framework.utils.PropsConst;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+
+public class BaseClass {
+    private final DriverManager driverManager = DriverManager.getInstance();
+    protected PageManager pageManager = PageManager.getInstance();
+    private final TestPropManager propManager = TestPropManager.getInstance();
+
+    @BeforeAll
+    public static void beforeClass(){
+        InitManager.initFramework();
+    }
+
+    @BeforeEach
+    void before() {
+        driverManager.getDriver().get(propManager.getProperty(PropsConst.BASE_URL));
+    }
+
+    @AfterAll
+    public static void afterAll() {
+        InitManager.quitFramework();
+    }
+
+}
