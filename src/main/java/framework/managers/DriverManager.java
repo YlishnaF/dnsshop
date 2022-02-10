6fb3b4beaccc93cdf117af2897e9a74cff9095b5
YlishnaF <ylishnaf@yandex.ru>
2022-02-10 11:01:06 +0300

    second commit

--- src/main/java/framework/managers/DriverManager.java
+++ src/main/java/framework/managers/DriverManager.java
@@ -3,6 +3,11 @@ package framework.managers;
 import framework.utils.PropsConst;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+
+import java.net.MalformedURLException;
+import java.net.URI;
 
 public class DriverManager {
     private static DriverManager INSTANCE;
@@ -28,8 +33,22 @@ public class DriverManager {
     }
 
     public void initDriver(){
-        System.setProperty("webdriver.chrome.driver", propManager.getProperty(PropsConst.PATH_CHROM_DRIVER_WINDOWS));
-        driver = new ChromeDriver();
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setBrowserName("chrome");
+        capabilities.setVersion("73.0");
+        capabilities.setCapability("enableVNC", true);
+        capabilities.setCapability("enableVideo", false);
+
+        try {
+            RemoteWebDriver driver = new RemoteWebDriver(
+                    URI.create("http://selenoid.appline.ru:4445/wd/hub").toURL(),
+                    capabilities
+            );
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        }
+//        System.setProperty("webdriver.chrome.driver", propManager.getProperty(PropsConst.PATH_CHROM_DRIVER_WINDOWS));
+//        driver = new ChromeDriver();
 
     }
     public void quitDriver(){
